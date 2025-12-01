019ee0874b1cc47d8c1bf6677418130725ef24e3
João Nascimento <joao.nascimento@beegin.com.br>
2025-12-01 12:53:36 -0300

    db connection

--- backend/settings.py
+++ backend/settings.py
@@ -18,7 +18,7 @@ import dj_database_url
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-DATABASE_URL = "postgresql://postgres:amSlbKvGTHodcLnHmvGSJdfhEotjDXxM@turntable.proxy.rlwy.net:12620/railway";
+DATABASE_URL = "postgresql://postgres:amSlbKvGTHodcLnHmvGSJdfhEotjDXxM@turntable.proxy.rlwy.net:12620/railway"
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
